2746f12124669dc1d871a093f545d981a7e52d32
wser <w.web.dev@gmail.com>
2021-02-17 15:43:45 +0100

    chng to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '', // to use relative paths in build
+  base: './', // to use relative paths in build
   plugins: [vue()]
 })
