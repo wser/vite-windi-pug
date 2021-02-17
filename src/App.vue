a7d7a5210eac22a00573bd2516e01bd7c6f474f8
wser <w.web.dev@gmail.com>
2021-02-17 16:44:15 +0100

    imported description from package.json

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 img(alt="Vue logo" src="./assets/logo-min.png")
 HelloWorld msg="Hello Vue 3 + Vite + PUG"
 p Hi
+b {{pjson.description}}
 </template>
 
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import pjson from '../package.json'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
